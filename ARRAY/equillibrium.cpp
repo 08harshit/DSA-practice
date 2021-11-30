9c0c2d99aeb9a2e4a4a942345b4ffc428b92caaa
08harshit <heyharshithere@gmail.com>
2021-11-30 16:46:30 +0530

    update

---
+++ ARRAY/equillibrium.cpp
@@ -0,0 +1,13 @@
+#include<iostream>                               //HARSHIT SEN
+using namespace std;
+void equillibrium(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+                         //prefix sum sikh liyo pehle        
+    }
+}
+int main(){
+    
+    return 0;
+}
---
+++ ARRAY/tripellets.cpp
@@ -0,0 +1,27 @@
+#include <iostream> //HARSHIT SEN
+using namespace std;
+void triplet(int arr[], int n)
+{
+    int count = 0;
+    for (int i = 0; i < n - 2; i++)
+    {
+        for (int j = i + 2; j < n; j++)
+        {
+            if (arr[i] + arr[i + 1] == arr[j])
+            {
+                count++;+
+                
+            }
+        }
+    }
+
+    cout << count;
+}
+
+int main()
+{
+    int n = 4;
+    int arr[] = {1, 2, 3, 5};
+    triplet(arr, n);
+    return 0;
+}
