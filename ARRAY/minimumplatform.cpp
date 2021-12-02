3d2bcd4d1b0e748caa38cf397562853811d40ac7
08harshit <heyharshithere@gmail.com>
2021-12-03 00:28:24 +0530

    update

---
+++ ARRAY/minimumplatform.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;      //HARSHIT SEN
+void platform(int ari[], int dep[], int n)
+{
+    int count[6] = {1, 1, 1, 1, 1, 1};
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (dep[i] > ari[j])
+                
+            {
+                count[i]++;
+            }
+        }
+    }
+    sort(count, count + 6);
+    cout << "Number of platforms required:" << count[5];
+}
+using namespace std;
+int main()
+{
+
+    int ari[] = {900, 1100, 1235};
+    int dep[] = {1000, 1200, 1240};
+    int n = sizeof(ari) / sizeof(ari[0]);
+    platform(ari, dep, n);
+    return 0;
+}
--- ARRAY/test.cpp
+++ ARRAY/test.cpp
@@ -1,27 +1,33 @@
-#include<iostream>
+#include<bits\stdc++.h>
 using namespace std;
-
-
-void leaders(int arr[], int size)
-{
-	for (int i = 0; i < size; i++)
-	{
-		int j;
-		for (j = i+1; j < size; j++)
-		{
-			if (arr[i] <=arr[j])
-				break;
-		}
-		if (j == size) 
-			cout << arr[i] << " ";
-}
-}
-
-
 int main()
 {
-	int arr[] = {16, 17, 4, 3, 5, 2};
-	int n = sizeof(arr)/sizeof(arr[0]);
-	leaders(arr, n);
-	return 0;
+	int i,arr1[] = {900, 940, 950, 1100, 1500, 1800},arr2[] = {910, 1200, 1120, 1130, 1900, 2000},j,count[6] = {1,1,1,1,1,1};
+//	cout<<"enter the size : "<<endl;
+//	cin>>n;
+//	cout<<"enter the elements of arrival : "<<endl;
+//	for(i=0;i<n;i++)
+//	{
+//		cin>>arr1[i];
+//	}
+//	cout<<"enter the elements departure : "<<endl;
+//	for(i=0;i<n;i++)
+//	{
+//		cin>>arr2[i];
+//	}
+//	for(i=0;i<n;i++)
+//	{
+//		count[i] = 1;
+//	}
+	for(i=0;i<6;i++)
+	{
+		for(j=i+1;j<6;j++)
+		{
+			if(arr2[i]>arr1[j])
+			count[i]++;
+		}
+	}
+	sort(count,count +6);
+
+	cout<<count[5];
 }
