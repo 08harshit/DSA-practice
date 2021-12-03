d84635eebe91878817ef9840208b07e27bd9e305
08harshit <heyharshithere@gmail.com>
2021-12-03 12:49:21 +0530

    update

---
+++ LinnkedList/reverse_LL.cpp
@@ -0,0 +1,66 @@
+#include <iostream> //HARSHIT SEN
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *link;
+} * head;
+void headinsert(int x)
+{
+    node *temp = new node;
+    temp->data = x;
+    temp->link = NULL;
+    if (head != NULL)
+    {
+        temp->link = head;
+    }
+    head = temp;
+}
+void reverse()
+{
+    node *current = new node;
+    node *prev = new node;
+    node *next = new node;
+    current = head;
+    prev = NULL;
+    while (current != NULL)
+    {
+        next=current->link;
+        current->link=prev;
+        prev=current;
+        current=next;
+
+    }
+head=prev;
+}
+void display()
+{
+    node *temp = new node;
+    temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data<<" ";
+        temp = temp->link;
+    
+    }
+cout<<endl;
+}
+int main()
+{
+    head = NULL;
+    int n, x;
+    cout << "how many elements:";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        headinsert(x);
+    }
+    cout<<"linked list:";
+    display();
+    reverse();
+    cout<<"after reversing:";
+    display();
+    return 0;
+}
